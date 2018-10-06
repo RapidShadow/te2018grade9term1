98dec0029fb629a8d1c84ab466abeda1dffd58b7
David Ung <Davidu@csedge.org>
2018-10-06 13:18:13 -0700

    Updated Data.js
    Alexa play Despacito

--- data.js
+++ data.js
@@ -1,5 +1,23 @@
 // Example
-let message = "Hello World!";
+let message = "Alexa play Despacito";
 console.log(message);
 
 // Type your code below!
+let name = "David";
+let friendA = "Jacob";
+let friendB = "Astrid";
+let bDayDay = 6;
+let bDayMonth = 7;
+let bDayYear = 2004;
+let jacobIsAWeeb = true;
+let astridIsAWeeb = true;
+let mySocialSecurityNumber = undefined;
+console.log(name);
+console.log(friendA);
+console.log(friendB);
+console.log(bDayDay);
+console.log(bDayMonth);
+console.log(bDayYear);
+console.log(jacobIsAWeeb);
+console.log(astridIsAWeeb);
+console.log(mySocialSecurityNumber);
