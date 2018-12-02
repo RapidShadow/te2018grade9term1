4e087e879d3f592c0b8a46814740ecd52217856f
David Ung <davidu@csedge.org>
2018-12-01 23:44:12 -0800

    Update haunted.js

--- haunted.js
+++ haunted.js
@@ -1,9 +1,8 @@
 const READLINE = require("readline-sync");
 
-// use whatever properties you want for the player object
+
 let player = {
-  name: undefined,
-  weapon: "fists"
+  name: undefined
 };
 
 console.log();
@@ -12,26 +11,53 @@ player.name = READLINE.question("What is your name?: ");
 console.log(`${player.name} Wakes up somewhere...`);
 console.log(`${player.name} Are you in the city or Are you in a farm?`);
 let city = READLINE.question("Do you wake up in a city? (yes or no) ");
-if( city == "y" || city == "yes") {
+   if(city == "n" || city == "no"){
+     console.log(`${player.name} wakes up...`);
+      let decision = READLINE.question("Do you feel cold or hot?")
+      if (decision == "hot"){
+       console.log("zombie insects bite and sting you die, Thanks for playing");
+      }
+      if (decision == "cold"){
+       let decision = READLINE.question("It is cold, do you start a fire?");
+       if(decision == "n" || decision == "no"){
+         console.log("You freeze over and you die, Thanks for playing!");
+       }
+       if(decision == "y" || decision == "yes"){
+         let decision = READLINE.question("you find a axe, and head out of firewood. A zombie sneaks up on you, do you fight or run?");
+         if (decision == "run"){
+          console.log("You fail to get firewood before the night, you freeze over and die. Thanks for playing! ");
+         }
+         if(decision == "fight"){
+           console.log("You chop the zombie in half. You survive in the farm by growing food and surviving long enough for the zombies to decompose. Thanks for Playing!");
+         }
+       }
+      }
+   }
+if(city == "y" || city == "yes") {
   console.log(`${player.name} wakes up...`);
   let decision  = READLINE.question("Would you like to visit the Gunshop (1) or a Lab (2)?");
-  if (decision === 2){
-   let decision = READLINE.question("You entered the lab.You find a bunch of machines.One is catching your eye, Do you turn it on?")
-    
+  if (decision == 2){
+      let decision = READLINE.question("You enter the lab. There is an eerie silence. You see something that catchs your eye, do you turn it on?");
+      if (decision == "yes" || decision == "y"){
+       console.log("A beam of light pierces the sky. All zombies get purifed and disintegrated. You win!, Thanks for Playing!");
+      }
+      if (decision == "no" || decision == "n"){
+       console.log("Zombies being to break through the door. They break through and you die, Thanks for Playing!");
+      }
+     }
+  if (decision == 1) {
+   let decision = READLINE.question("The Gunshop is empty is would you like a Assault Rifle?(yes or no)");
+   if (decision === "y" || decision === "yes") {
+     let decision = READLINE.question("There is a horde of zombies apporaching you, do you run or fight?");
+     if (decision == "run"){
+      console.log("you die trying to run, Thanks for playing!");
   }
-  if (decision === 1) {
-   let decision = READLINE.question("The Gunshop is empty is would you like a Assualt Rifle?(yes or no)");
-    if (decision === "y" || decision === "yes") {
-      let decision = READLINE.question("There is a horde of zombies apporaching you, do you run or fight?");
+     if (decision == "fight"){
+      console.log("you fight your way out and live long enough for the zombies to decompose, Thanks for playing!");
+     }
+  }
+  if (decision == "n" || decision == "no"){
+   console.log("You walk outside and a horde of zombies kill you. Thanks for playing!");
     }
-
-
  }
-}  // continue the story
-
-//} else {
-  //console.log(`${player.name} decides not to open the door. However...`);
-  // continue the story
-
-//}
-//console.log("Thanks for playing!");
+}
